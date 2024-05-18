77defa296d7256e840a9fff8be48abd3f3985f80
Ahmet Kilinc <akx9@icloud.com>
2024-05-18 21:28:23 +0100

    add relationTo for user to media

--- dev/src/collections/Users.ts
+++ dev/src/collections/Users.ts
@@ -1,15 +1,20 @@
-import { CollectionConfig } from 'payload/types';
+import { CollectionConfig } from "payload/types";
 
 const Users: CollectionConfig = {
-  slug: 'users',
-  auth: true,
-  admin: {
-    useAsTitle: 'email',
-  },
-  fields: [
-    // Email added by default
-    // Add more fields as needed
-  ],
+	slug: "users",
+	auth: true,
+	admin: {
+		useAsTitle: "email",
+	},
+	fields: [
+		// Email added by default
+		// Add more fields as needed
+		{
+			type: "upload",
+			name: "photo",
+			relationTo: "media",
+		},
+	],
 };
 
 export default Users;
