80331e7ff375587fc8c3a668d67066af54ccfd3c
Ahmet Kilinc <akx9@icloud.com>
2024-05-22 21:03:09 +0100

    feat: show doc icon if not media type (image/video)

--- src/components/MediaGrid/MediaGrid.client.tsx
+++ src/components/MediaGrid/MediaGrid.client.tsx
@@ -147,78 +147,121 @@ const MediaGridClient = () => {
 						enableColumns={false}
 					/>
 					{BeforeListTable}
-					<div className={`${baseClass}__sorting-header`}>
-						<SelectAll />
-						<SortColumn Label="File Name" name="filename" />
-						<SortColumn Label="Alt Tag" name="alt" />
-						<SortColumn Label="Created At" name="createdAt" />
-						<SortColumn Label="Updated At" name="updatedAt" />
-					</div>
-					<div className={`${baseClass}__grid`}>
-						{data.docs
-							? data.docs.map((doc: Media) => (
-									<Button
-										key={doc.id}
-										className={`${baseClass}__grid-card`}
-										to={`${adminRoute}/collections/media/${doc.id}`}
-										el="link"
-										Link={Link}
-										buttonStyle="none"
+					{data.docs.length ? (
+						<div className={`${baseClass}__sorting-header`}>
+							<SelectAll />
+							<SortColumn Label="File Name" name="filename" />
+							<SortColumn Label="Alt Tag" name="alt" />
+							<SortColumn Label="Created At" name="createdAt" />
+							<SortColumn Label="Updated At" name="updatedAt" />
+						</div>
+					) : null}
+					{data.docs && data.docs.length > 0 && (
+						<div className={`${baseClass}__grid`}>
+							{data.docs.map((doc: Media) => (
+								<Button
+									key={doc.id}
+									className={`${baseClass}__grid-card`}
+									to={`${adminRoute}/collections/${collectionSlug}/${doc.id}`}
+									el="link"
+									Link={Link}
+									buttonStyle="none"
+								>
+									<div
+										className={`${baseClass}__select`}
+										onClick={(e) => e.stopPropagation()}
 									>
+										{/* TODO: Selecting single row doesnt work as cant provide ID to it now it is provided by provider. */}
+										<SelectRow />
+									</div>
+									{doc.mimeType?.includes("image") ? (
+										<img
+											src={doc.url!}
+											className={`${baseClass}__grid-media`}
+										/>
+									) : doc.mimeType?.includes("video") ? (
+										<video
+											src={doc.url!}
+											autoPlay={true}
+											muted={true}
+											loop={true}
+											playsInline={true}
+											controls={false}
+											className={`${baseClass}__grid-media`}
+										/>
+									) : (
 										<div
-											className={`${baseClass}__select`}
-											onClick={(e) => e.stopPropagation()}
+											className={`thumbnail ${baseClass}__grid-media`}
 										>
-											{/* TODO: Selecting single row doesnt work as cant provide ID to it now it is provided by provider. */}
-											<SelectRow />
-										</div>
-										{doc.mimeType?.includes("image") ? (
-											<img
-												src={doc.url!}
-												className={`${baseClass}__grid-media`}
-											/>
-										) : (
-											<video
-												src={doc.url!}
-												autoPlay={true}
-												muted={true}
-												loop={true}
-												playsInline={true}
-												controls={false}
-												className={`${baseClass}__grid-media`}
-											/>
-										)}
-										<div
-											className={`${baseClass}__grid-meta`}
-										>
-											<p>
-												<span
-													className={`${baseClass}__grid-title`}
-												>
-													{doc.filename}
-												</span>
-												{" - "}
-												{formatFilesize(doc.filesize!)}
-											</p>
-											<p
-												className={`${baseClass}__grid-alt`}
+											<svg
+												height="150"
+												viewBox="0 0 150 150"
+												width="150"
+												xmlns="http://www.w3.org/2000/svg"
 											>
-												{doc.alt || "<No Alt Text>"}
-											</p>
-											<p
-												className={`${baseClass}__grid-alt`}
-											>
-												{formatDate({
-													date: doc.createdAt,
-													pattern: dateFormat,
-													i18n: i18n,
-												})}
-											</p>
+												<rect
+													fill="#333333"
+													height="150"
+													transform="translate(0 0.5)"
+													width="150"
+												></rect>
+												<path
+													d="M82.8876 50.5H55.5555V100.5H94.4444V61.9818H82.8876V50.5Z"
+													fill="white"
+												></path>
+												<path
+													d="M82.8876 61.9818H94.4444L82.8876 50.5V61.9818Z"
+													fill="#9A9A9A"
+												></path>
+											</svg>
 										</div>
-									</Button>
-								))
-							: null}
-					</div>
+									)}
+									<div className={`${baseClass}__grid-meta`}>
+										<p>
+											<span
+												className={`${baseClass}__grid-title`}
+											>
+												{doc.filename}
+											</span>
+											{" - "}
+											{formatFilesize(doc.filesize!)}
+										</p>
+										<p className={`${baseClass}__grid-alt`}>
+											{formatDate({
+												date: doc.createdAt,
+												pattern: dateFormat,
+												i18n: i18n,
+											})}
+										</p>
+									</div>
+								</Button>
+							))}
+						</div>
+					)}
+
+					{data.docs && data.docs.length === 0 && (
+						<div className={`${payloadBaseClass}__no-results`}>
+							<p>
+								{i18n.t("general:noResults", {
+									label: getTranslation(labels?.plural, i18n),
+								})}
+							</p>
+							{hasCreatePermission && newDocumentURL && (
+								<Button
+									Link={Link}
+									el="link"
+									to={newDocumentURL}
+								>
+									{i18n.t("general:createNewLabel", {
+										label: getTranslation(
+											labels?.singular,
+											i18n,
+										),
+									})}
+								</Button>
+							)}
+						</div>
+					)}
 					{AfterListTable}
 					{data.docs && data.docs.length > 0 && (
 						<div className={`${payloadBaseClass}__page-controls`}>
